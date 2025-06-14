2a7e29f4245ecbf7ccbc0b586b6ecd557ca507bb
Prisma muharrom <prisma.muharrom@jelajahdata.co.id>
2025-06-14 15:45:02 +0700

    adding test suite

---
+++ Test Suites/PracticeFormSuite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>PracticeFormSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a2460aaf-ea6f-408b-9423-5c098699690d</testSuiteGuid>
+   <testCaseLink>
+      <guid>ece1bc1c-0459-4e47-86fd-0bf620acdf5e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-PracticeForm</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
